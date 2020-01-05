6678a9ec8b6828f62e3aea4a1df01c2bd7d1ae28
mplouffe <matheuplouffe@gmail.com>
2020-01-05 10:52:44 -0600

    Using v-bind to bind attribute to data properties.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         bot: 'Robot_#000',
-        description: 'A humanoid machine'
+        description: 'A humanoid machine',
+        image: './assets/vueBot.png',
+        link: 'linkurlsss'
     }
 });
