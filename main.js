bb3e89ac5085f23f3362dfba171564840d0d150d
mplouffe <matheuplouffe@gmail.com>
2020-01-06 22:27:11 -0600

    Added event handling and cart.

--- main.js
+++ main.js
@@ -3,20 +3,31 @@ var app = new Vue({
     data: {
         bot: 'Robot_#000',
         description: 'A humanoid machine',
-        image: './assets/vueBot.png',
+        image: './assets/vueBot000.png',
         link: 'linkurl',
         incompleteParts: 10,
         details: ["100 Power", "50 Weight", "30 Speed"],
         variants: [
             {
                 variantId: 000,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: './assets/vueBot000.png'
             },
             {
                 variantId: 001,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: './assets/vueBot001.png'
             }
-        ]
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        setBotImage(variantImage) {
+            this.image = variantImage;
+        }
     }
 });
 
